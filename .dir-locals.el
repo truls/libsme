45344cfbb3dc7fcaeced1d03bf19f31379c84fc4
Truls Asheim <truls@asheim.dk>
2018-04-24 04:18:16 +0200

    Intero config

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((haskell-mode
-  (intero-targets "libsme:lib" "libsme:exe:smec" "libsme:test:libsme-test")))
+  (intero-targets "libsme:lib" "libsme:exe:smec")))
+
